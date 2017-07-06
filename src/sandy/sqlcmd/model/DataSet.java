12c172c0c57fa47f9dcffed65685b88b78555055
Aleksandr Shmarov <sandy.dn.ua@gmail.com>
2017-07-06 23:26:37 +0300

    2

--- src/sandy/sqlcmd/model/DataSet.java
+++ src/sandy/sqlcmd/model/DataSet.java
@@ -6,95 +6,93 @@ import java.util.Iterator;
 public class DataSet {
     private ArrayList<ArrayList<String>> table;
     private ArrayList<String> text;
-    public int getSizeTable(){
+
+    public int getSizeTable() {
         return table.size();
     }
 
-    public DataSet(){
+    public DataSet() {
         text = new ArrayList<>();
         table = new ArrayList<>();
     }
 
-    public DataSet(String srt){
+    public DataSet(String srt) {
         text = new ArrayList<>();
         table = new ArrayList<>();
         addString(srt);
     }
-    public Iterator<String> iteratorText(){
+
+    public Iterator<String> iteratorText() {
         return text.iterator();
     }
-    /*public Iterator<String> iteratorFieldsInRow(int i){
 
-        if(i >= 0 && i < quantityRows()){
-            return table.get(i).iterator();
-        }else{
-            return null;
-        }
-    }
-*/
-    public int addRow(String str){
+    public int addRow(String str) {
         int i = addRow();
         addField(i, str);
-        return table.size()-1;
+        return table.size() - 1;
     }
 
-    public void addString(String str){
+    public void addString(String str) {
         text.add(str);
     }
-    public void addString(String[] strings){
-        for (String s : strings){
+
+    public void addString(String[] strings) {
+        for (String s : strings) {
             addString(s);
         }
     }
 
-    public int addRow(){
+    public int addRow() {
         table.add(new ArrayList<String>());
-        return table.size()-1;
+        return table.size() - 1;
     }
 
-    public void addRows(String[] strings){
-        for (String s : strings){
+    public void addRows(String[] strings) {
+        for (String s : strings) {
             addRow(s);
         }
     }
 
-    public int addRow(ArrayList<String> row){
+    public int addRow(ArrayList<String> row) {
         table.add(row);
-        return table.size()-1;
+        return table.size() - 1;
     }
 
-    public void addField(int indexRow,String field){
-        if(existingIndexRow(indexRow)){
+    public void addField(int indexRow, String field) {
+        if (existingIndexRow(indexRow)) {
             table.get(indexRow).add(field);
-        }else{
+        } else {
         }
     }
 
-    public String getField(int i,int j){
-        if( existingIndexRow(i) && j < table.get(i).size() && j >= 0){
+    public String getField(int i, int j) {
+        if (existingIndexRow(i) && j < table.get(i).size() && j >= 0) {
             return table.get(i).get(j);
-        }else{
+        } else {
             return null;
         }
     }
 
-    public int quantityRows(){
+    public int quantityRows() {
         return table.size();
     }
-    public int QuantityStrings(){
+
+    public int QuantityStrings() {
         return text.size();
     }
-    public String getString(int i){
-        if( i >= 0 && i < text.size()){
+
+    public String getString(int i) {
+        if (i >= 0 && i < text.size()) {
             return text.get(i);
-        }else{
-            return  null;
+        } else {
+            return null;
         }
     }
-    public int quantityFieldsInRow(int i){
-        if(existingIndexRow(i)){
+
+    public int quantityFieldsInRow(int i) {
+        if (existingIndexRow(i)) {
             return table.get(i).size();
-        }else{
+        } else {
             return 0;
         }
     }
